a30b642edc1aa10741622d7b660f024a0c6199b3
jianphu <jianphu@qq.com>
2014-04-08 11:03:06 +0800

    删除序号文件

--- 01.ubuntu_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install subversion git -y
-sudo apt-get install zip unzip -y
-wget https://nodeload.github.com/goagent/goagent/legacy.zip/3.0 -o goagent-3.17.zip
-unzip goagent-3.17.zip
